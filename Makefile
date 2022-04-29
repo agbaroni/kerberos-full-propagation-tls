a0646961ba5c1879f30e6425d23625ab8a3b6186
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2022-04-29 14:25:17 +0200

    Fixed Makefile cleaning

--- Makefile
+++ Makefile
@@ -8,6 +8,12 @@ all: clean images
 	$(PODMAN) run --name $(IC_PREFIX)tls --rm --volume $(PWD)/tls/artifacts:/tmp/artifacts $(IC_PREFIX)tls
 
 clean:
+	-$(RM) tls/artifacts/*.crt
+	-$(RM) tls/artifacts/*.csr
+	-$(RM) tls/artifacts/*.key
+	-$(RM) tls/artifacts/index.txt.*
+	-$(RM) tls/artifacts/serial
+	-$(RM) tls/artifacts/certs/*.pem
 	-$(PODMAN) rm -f $(IC_PREFIX)openldap
 	-$(PODMAN) rm -f $(IC_PREFIX)tls
 	-$(BUILDAH) rmi $(IC_PREFIX)openldap
--- tls/run.sh
+++ tls/run.sh
@@ -4,4 +4,8 @@ set -x
 
 cd /tmp/artifacts
 
-openssl3 req -passout 'pass:password' -out ca.crt -new -keyout ca.key -config /tmp/openssl.cnf -subj '/C=IT/O=Example/CN=CA' -x509
+openssl3 req -passout 'pass:password' -out ca.csr -new -keyout ca.key -config /tmp/openssl.cnf -subj "/C=IT/O=Example/CN=CA"
+openssl3 ca -config /tmp/openssl.cnf -in ca.csr -passin 'pass:password' -out ca.crt -create_serial -selfsign -keyfile ca.key -batch
+
+openssl3 req -passout 'pass:password' -out openldap.csr -new -keyout openldap.key -config /tmp/openssl.cnf -subj "/C=IT/O=Example/CN=kfp-openldap"
+openssl3 ca -config /tmp/openssl.cnf -in openldap.csr -passin 'pass:password' -out openldap.crt -batch
